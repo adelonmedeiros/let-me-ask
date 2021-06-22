c825ea0dec755afea51722975a397f3648feeb57
Adelon Medeiros <adelonmedeiros@pop-os.localdomain>
2021-06-22 01:22:01 -0300

    second  day

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import { Button } from './components/Button'
+import { createContext } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { Home } from './pages/Home';
+import { NewRoom } from "./pages/NewRoom";
+
+const TestContext = createContext('');
 
 function App() {
   return (
-   <Button />
+    <BrowserRouter>
+    <TestContext.Provider value={'Teste'}>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+      </TestContext.Provider>
+    </BrowserRouter>
   );
 }
 
